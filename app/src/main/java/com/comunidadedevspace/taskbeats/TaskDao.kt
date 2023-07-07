2f295e5981639f2ca3aa9a5754a120e7750c1c6b
jefersonassis <jefersonasssis@icloud.com>
2023-07-07 20:56:15 +0200

    Initial Commit

---
+++ app/src/main/java/com/comunidadedevspace/taskbeats/TaskDao.kt
@@ -0,0 +1,30 @@
+package com.comunidadedevspace.taskbeats
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+
+@Dao
+interface TaskDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insert (task: Task)
+
+    @Query("Select * from task")
+    fun getAll():List<Task>
+
+    //Update - encontrar a tarefa que queremos mudar
+
+    @Update(onConflict = OnConflictStrategy.REPLACE)
+    fun update(task:Task)
+    //Delete - to delete we got to find the task by an ID
+
+    @Query("Delete from task WHERE id =:id")
+    fun deleteById(id:Int)
+
+    @Query("Delete from task")
+    fun deleteAll()
+}
